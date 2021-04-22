f5c53659c28cbb04fdfa4ee0159b0ff2e8ba56b1
Phung Phu <phungphu@uw.edu>
2021-04-22 11:37:14 -0700

    Answer questions

--- analysis.R
+++ analysis.R
@@ -4,6 +4,7 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(lintr)
 
 # Create the `charts/` directory (you can do this from R!)
 dir.create("charts", showWarnings = FALSE)
@@ -11,10 +12,10 @@ dir.create("charts", showWarnings = FALSE)
 # Load prepped data
 health_data <- read.csv("./data/prepped/all_data.csv")
 
-# Are HALE and life expectancy correlated?
+############### Are HALE and life expectancy correlated? ###############
 # - Plot 2016 life expectancy against 2016 HALE. Save the graph to `charts/`
 # - Compute the correlation between 2016 life expectancy against 2016 HALE
-data_2016 <- health_data %>% 
+data_2016 <- health_data %>%
   filter(year == 2016)
 
 ggplot(data_2016) +
@@ -26,9 +27,13 @@ ggplot(data_2016) +
 ggsave("charts/le_hale_graph.png")
 
 cor(data_2016$hale, data_2016$le)
-# 0.9957015. as le increases, hale also increases at a similar rate
+# The correlation between life expectancy and HALE is 0.9957015. This indicates
+# that there is a strong correlation between life expectancy and HALE because
+# the correlation is almost equal to 1,  so as life expectancy increases, HALE
+# also increases at a similar rate.
 
-# Are HALE and DALYs correlated?
+
+################ Are HALE and DALYs correlated? ################
 # - Plot 2016 HALE against 2016 DALYs. Save the graph to `charts/`
 # - Compute the correlation between 2016 HALE and DALYs
 ggplot(data_2016) +
@@ -41,9 +46,13 @@ ggsave("charts/dalys_hale_graph.png")
 
 cor(data_2016$dalys, data_2016$hale)
 
-#  -0.9859484. as dalys increase, HALE decreases at a similar rate
+# The correlation between HALE and DALYs is -0.9859484. This indicates that
+# there is a strong correlation between HALE and DALYs since the correlation is
+# almost equal to 1. As DALYs increases, HALE decreases at a similar rate.
 
-# As people live longer, do they live healthier lives 
+
+
+############ As people live longer, do they live healthier lives ###############
 # (i.e., is a smaller fraction of life spent in poor health)?
 # Follow the steps below to attempt to answer this question.
 
@@ -51,12 +60,12 @@ cor(data_2016$dalys, data_2016$hale)
 # This will create `hale_2016`, `hale_1990`, `le_2016`, etc.
 # To do this, I suggest that you use the `pivot` function in the new
 # tidyverse release:https://tidyr.tidyverse.org/articles/pivot.html#wider
-data_wide <- health_data %>% 
+data_wide <- health_data %>%
   pivot_wider(names_from = year,
               values_from = c(hale, le, dalys))
 
 # Create columns to store the change in life expectancy, and change in hale
-data_wide <- data_wide %>% 
+data_wide <- data_wide %>%
   mutate(hale_diff = hale_2016 - hale_1990,
           le_diff = le_2016 - le_1990)
 
@@ -74,6 +83,11 @@ ggplot(data_wide) +
 
 ggsave("charts/change.png")
 
-# 
-  
-
+# The chart indicates that as the change in life expectancy increases, the
+# change in HALE also increases since there is a positive, almost linear, slope.
+# As people live longer, they do live healthier lives. However, even though
+# the change in HALE increases as the change in life expectancy increases, the
+# rate for the change in HALE slows down after the change in life expectancy
+# reaches around 5 because the points do not align with the 45 degree line. So
+# as the change in life expectancy increases, the change in HALE does not
+# increase as much as the rate when the change in life expectancy is lower.
